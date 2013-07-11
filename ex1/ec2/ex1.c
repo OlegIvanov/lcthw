4af333557366c1ab31442b1a484d0b2e55d290ff
Oleg Ivanov <iosmsft@outlook.com>
2013-07-11 15:24:39 +0300

    ex1 ec2

---
+++ ex1/ec2/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("Something more complex than hello world.");
+	puts("First line.");
+	puts("Second line.");
+	puts("Third line.");
+	puts("Last fourth line.");
+	return 0;
+}
