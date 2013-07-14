a91ebc7e07e6edb2f2cefa8d710d9ea01822adcc
Oleg Ivanov <iosmsft@outlook.com>
2013-07-14 23:59:38 +0300

    ex12 ec1

--- ex12/ec1/ex12.c
+++ ex12/ec1/ex12.c
@@ -3,10 +3,15 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
-
+	
+	// Write a few more test cases for this program to see what you can come up with.
 	if (argc == 1) {
 		printf("You only have one argument. You suck.\n");
-	} else if(argc > 1 && argc < 4) {
+	} 
+	else if(argc > 1 && argc < 4) {
+		if(argc != 3) {
+			printf("I am so relieved!\n");
+		}
 		printf("Here's your arguments:\n");
 		for(i = 0; i < argc; i++) {
 			printf("%s ", argv[i]);
@@ -15,6 +20,9 @@ int main(int argc, char *argv[])
 	} else {
 		printf("You have too many arguments. You suck.\n");
 	}
+	
+	if (argc % 2 == 1)
+		printf("Piss coming from my ass. Suck it, bitch.\n");
 
 	return 0;
 }
