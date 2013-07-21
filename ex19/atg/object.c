ab09c25fd3eeeb77afb1c0e28494311a7e84c7b5
Oleg Ivanov <iosmsft@outlook.com>
2013-07-21 20:52:35 +0300

    ex19 atg

--- ex19/atg/object.c
+++ ex19/atg/object.c
@@ -40,6 +40,8 @@ int Object_attack(void *self, int damage)
 
 void *Object_new(size_t size, Object proto, char *description)
 {
+	assert(description != NULL);
+
 	// setup the default functions in case they aren't set
 	if(!proto.init) proto.init = Object_init;
 	if(!proto.describe) proto.describe = Object_describe;
@@ -50,10 +52,15 @@ void *Object_new(size_t size, Object proto, char *description)
 	// this seems weird, but we can make a struct of one size,
 	// then point a different pointer at it to "cast" it
 	Object *el = calloc(1, size);
+
+	assert(el != NULL);
+	
 	*el = proto;
 
 	// copy the description over
 	el->description = strdup(description);
+
+	assert(el->description);
 
 	// initialize it with whatever init we were given
 	if(!el->init(el)) {
