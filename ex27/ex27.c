0d6d2a03ed6871b92ab24bee90146cf5b84ee94f
Oleg Ivanov <iosmsft@outlook.com>
2013-07-28 13:28:57 +0300

    ex27

--- ex27/ex27.c
+++ ex27/ex27.c
@@ -23,7 +23,7 @@ void copy(char to[], char from[])
  */
 int safercopy(int from_len, char *from, int to_len, char *to)
 {
-	assert(from != NULL && to != NULL && "from and to can't be nULL");
+	assert(from != NULL && to != NULL && "from and to can't be NULL");
 	int i = 0;
 	int max = from_len > to_len - 1 ? to_len - 1 : from_len;
 
