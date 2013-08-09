29701d95005b41199936b744fa4f71404b4672d3
Oleg Ivanov <iosmsft@outlook.com>
2013-08-10 01:19:42 +0300

    ex35 ec1

--- ex35/liblcthw/src/lcthw/darray_algos.c
+++ ex35/liblcthw/src/lcthw/darray_algos.c
@@ -1,17 +1,62 @@
 #include <lcthw/darray_algos.h>
 #include <stdlib.h>
+#include <lcthw/dbg.h>
 
 int DArray_qsort(DArray *array, DArray_compare cmp)
 {
 	qsort(array->contents, DArray_count(array), sizeof(void *), cmp);
 	return 0;
 }
-/*
-int DArray_heapsort(DArray *array, DArray_compare cmp)
+
+static inline int get_parent_index(int child_index)
 {
-	return heapsort(array->contents, DArray_count(array), sizeof(void *), cmp);
+	return (child_index % 2) ? (child_index - 1) / 2 : (child_index - 2) / 2;
 }
 
+int DArray_heapsort(DArray *array, DArray_compare cmp)
+{
+	check(array, "argument 'array' can't be NULL");
+
+	if(DArray_count(array) <= 1) {
+		return 0;
+	}
+
+	int i = 0, j = 0, current_index = 0, parent_index = 0;
+	void *current_value = NULL, *parent_value = NULL, *buf_value = NULL;
+
+	for(i = 0; i < DArray_count(array) - 1; i++) {
+
+		for(j = 1; j < DArray_count(array) - i; j++) {
+
+			current_index = j;
+			parent_index = get_parent_index(current_index);
+
+			current_value = DArray_get(current_index);
+			parent_value = DArray_get(parent_index);
+
+			while(cmp(current_value, parent_value) > 0) {
+
+				DArray_set(array, current_index, parent_value);
+				DArray_set(array, parent_index, current_value);
+
+				current_index = parent_index;
+				parent_index = get_parent_index(current_index);
+
+				current_value = DArray_get(current_index);
+				parent_value = DArray_get(parent_index);
+			}
+		}
+
+		buf_value = DArray_first(array);
+		DArray_set(array, 0, DArray_get(array, j - 1));
+		DArray_set(array, j - 1, buf_value);
+	}
+
+	return 0;
+error:
+	return -1;
+}
+/*
 int DArray_mergesort(DArray *array, DArray_compare cmp)
 {
 	return mergesort(array->contents, DArray_count(array), sizeof(void *), cmp);
--- ex35/liblcthw/tests/darray_algos_tests.c
+++ ex35/liblcthw/tests/darray_algos_tests.c
@@ -51,12 +51,12 @@ char *test_qsort()
 {
 	return run_sort_test(DArray_qsort, "qsort");
 }
-/*
+
 char *test_heapsort()
 {
 	return run_sort_test(DArray_heapsort, "heapsort");
 }
-
+/*
 char *test_mergesort()
 {
 	return run_sort_test(DArray_mergesort, "mergesort");
@@ -67,8 +67,8 @@ char *all_tests()
 	mu_suite_start();
 
 	mu_run_test(test_qsort);
-	/*
 	mu_run_test(test_heapsort);
+	/*
 	mu_run_test(test_mergesort);
 	*/
 	return NULL;
