18cab55a1f18dd387ae57edab49736285295b33b
Oleg Ivanov <iosmsft@outlook.com>
2013-08-12 22:42:31 +0300

    ex35 ec1

--- ex35/ec1/liblcthw/tests/darray_algos_tests.c
+++ ex35/ec1/liblcthw/tests/darray_algos_tests.c
@@ -1,5 +1,15 @@
 #include "minunit.h"
 #include <lcthw/darray_algos.h>
+#include <time.h>
+
+#define BILLION 1000000000UL
+
+#define ARRAYS_COUNT 1000000
+
+static unsigned long get_diff(struct timespec start, struct timespec end)
+{
+	return (unsigned long)(end.tv_sec - start.tv_sec) * BILLION + (unsigned long)(end.tv_nsec - start.tv_nsec);
+}
 
 int testcmp(char **a, char **b)
 {
@@ -68,6 +78,58 @@ char *test_quicksort()
 	return run_sort_test(DArray_quicksort, "quicksort");
 }
 
+char *test_algo_perfomance(int (*func)(DArray *, DArray_compare), const char *name)
+{
+	struct timespec start, end;
+	double diff;
+
+	DArray *arrays[ARRAYS_COUNT] = {NULL};
+
+	int i = 0;
+
+	for(i = 0; i < ARRAYS_COUNT; i++) {
+		arrays[i] = create_words();
+	}
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+
+	for(i = 0; i < ARRAYS_COUNT; i++) {
+		func(arrays[i], (DArray_compare)testcmp);
+	}
+
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+
+	diff = (double)get_diff(start, end) / ARRAYS_COUNT;
+	printf("\nAlgorithm %s took %lf nanoseconds to run.\n\n", name, diff);
+
+	for(i = 0; i < ARRAYS_COUNT; i++) {
+		mu_assert(is_sorted(arrays[i]), "didn't sort it");
+		DArray_destroy(arrays[i]);
+	}
+
+	return NULL;
+}
+
+char *test_qsort_perfomance()
+{
+	return test_algo_perfomance(DArray_qsort, "qsort (original)");
+}
+
+char *test_heapsort_perfomance()
+{
+	return test_algo_perfomance(DArray_heapsort, "heapsort (custom)");
+}
+
+char *test_mergesort_perfomance()
+{
+	return test_algo_perfomance(DArray_mergesort, "mergesort (custom)");
+}
+
+char *test_quicksort_perfomance()
+{
+	return test_algo_perfomance(DArray_quicksort, "quicksort (custom)");
+}
+
 char *all_tests()
 {
 	mu_suite_start();
@@ -76,6 +138,11 @@ char *all_tests()
 	mu_run_test(test_heapsort);
 	mu_run_test(test_mergesort);
 	mu_run_test(test_quicksort);
+
+	mu_run_test(test_qsort_perfomance);
+	mu_run_test(test_heapsort_perfomance);
+	mu_run_test(test_mergesort_perfomance);
+	mu_run_test(test_quicksort_perfomance);
 
 	return NULL;
 }
