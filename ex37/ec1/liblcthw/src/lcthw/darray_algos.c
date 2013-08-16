4ef22ee25155cef8af9f80c8a75d5374ce30aeb0
Oleg Ivanov <iosmsft@outlook.com>
2013-08-16 18:20:52 +0300

    ex37 ec1

---
+++ ex37/ec1/liblcthw/src/lcthw/darray_algos.c
@@ -0,0 +1,211 @@
+#include <lcthw/darray_algos.h>
+#include <stdlib.h>
+#include <lcthw/dbg.h>
+
+int DArray_qsort(DArray *array, DArray_compare cmp)
+{
+	qsort(array->contents, DArray_count(array), sizeof(void *), cmp);
+	return 0;
+}
+
+static inline int get_parent_index(int child_index)
+{
+	check(child_index > 0, "'child_index' can't be '0' because it's root index");
+
+	return (child_index % 2) ? (child_index - 1) / 2 : (child_index - 2) / 2;
+error:
+	return -1;
+}
+
+int DArray_heapsort(DArray *array, DArray_compare cmp)
+{
+	check(array, "argument 'array' can't be NULL");
+
+	if(DArray_count(array) <= 1) {
+		return 0;
+	}
+
+	int i = 0, j = 0, current_index = 0, parent_index = 0;
+	void *current_value = NULL, *parent_value = NULL, *buf_value = NULL;
+
+	for(i = 0; i < DArray_count(array) - 1; i++) {
+
+		for(j = 1; j < DArray_count(array) - i; j++) {
+
+			current_index = j;
+			parent_index = get_parent_index(current_index);
+
+			current_value = DArray_get(array, current_index);
+			parent_value = DArray_get(array, parent_index);
+
+			while(cmp(&current_value, &parent_value) > 0) {
+
+				DArray_set(array, current_index, parent_value);
+				DArray_set(array, parent_index, current_value);
+
+				// if "root" has been reached
+				if(parent_index == 0) break;
+
+				current_index = parent_index;
+				parent_index = get_parent_index(current_index);
+
+				current_value = DArray_get(array, current_index);
+				parent_value = DArray_get(array, parent_index);
+			}
+		}
+
+		buf_value = DArray_first(array);
+		DArray_set(array, 0, DArray_get(array, j - 1));
+		DArray_set(array, j - 1, buf_value);
+	}
+
+	return 0;
+error:
+	return -1;
+}
+
+static inline int DArray_merge(DArray *array, int start_left_index, int left_count, int right_count, DArray_compare cmp)
+{
+	check(array, "argument 'array' can't be NULL");
+
+	DArray *merged = DArray_create(1, left_count + right_count);
+
+	int i = 0, left_first_index = 0, right_first_index = 0, left_last_index = 0, right_last_index = 0;
+
+	void *left_first_value = NULL, *right_first_value = NULL;
+
+	left_first_index = start_left_index;
+	left_last_index = start_left_index + left_count - 1;
+
+	right_first_index = start_left_index + left_count;
+	right_last_index = start_left_index + left_count + right_count - 1;
+
+	for(i = 0; i < left_count + right_count; i++) {
+
+		if(left_first_index <= left_last_index && right_first_index <= right_last_index) {
+
+			left_first_value = DArray_get(array, left_first_index);
+			right_first_value = DArray_get(array, right_first_index);
+
+			if(cmp(&left_first_value, &right_first_value) <= 0) {
+
+				DArray_push(merged, left_first_value);
+				left_first_index++;
+
+			} else {
+
+				DArray_push(merged, right_first_value);
+				right_first_index++;
+			}
+
+		} else if(left_first_index <= left_last_index) {
+
+			left_first_value = DArray_get(array, left_first_index);
+			DArray_push(merged, left_first_value);
+			left_first_index++;
+
+		} else if(right_first_index <= right_last_index) {
+			
+			right_first_value = DArray_get(array, right_first_index);
+			DArray_push(merged, right_first_value);
+			right_first_index++;
+		}
+	}
+
+	for(i = 0; i < left_count + right_count; i++) {
+		DArray_set(array, start_left_index + i, DArray_get(merged, i));
+	}
+
+	DArray_destroy(merged);
+	
+	return 0;
+error:
+	return -1;
+}
+
+static int DArray_mergesort_utility(DArray *array, int start_left_index, int count, DArray_compare cmp)
+{
+	check(array, "argument 'array' can't be NULL");
+
+	if(count <= 1) return 0;
+
+	int left_count = count / 2;
+	int right_count = count - left_count;
+
+	int start_right_index = start_left_index + left_count;
+
+	DArray_mergesort_utility(array, start_left_index, left_count, cmp);
+	DArray_mergesort_utility(array, start_right_index, right_count, cmp);
+
+	DArray_merge(array, start_left_index, left_count, right_count, cmp);
+
+	return 0;
+error:
+	return -1;
+}
+
+int DArray_mergesort(DArray *array, DArray_compare cmp)
+{
+	return DArray_mergesort_utility(array, 0, DArray_count(array), cmp);
+}
+
+static inline int DArray_partition(DArray *array, int first_index, int last_index, DArray_compare cmp)
+{
+	check(last_index - first_index > 0, "subarray length must be >= 2");
+
+	int count = last_index - first_index + 1;
+
+	int pivot_index = first_index + count / 2;
+	void *pivot_value = DArray_get(array, pivot_index);
+
+	DArray_set(array, pivot_index, DArray_get(array, last_index));
+	DArray_set(array, last_index, pivot_value);
+
+	int i = 0;
+	void *i_value = NULL;
+	int new_pivot_index = first_index;
+	
+	for(i = first_index; i < last_index; i++) {
+		i_value = DArray_get(array, i);
+
+		if(cmp(&i_value, &pivot_value) < 0) {
+			if(i != new_pivot_index) {
+				DArray_set(array, i, DArray_get(array, new_pivot_index));
+				DArray_set(array, new_pivot_index, i_value);
+			}
+			
+			new_pivot_index++;
+		}
+	}
+
+	void *buf_value = DArray_get(array, new_pivot_index);
+	DArray_set(array, new_pivot_index, DArray_get(array, last_index));
+	DArray_set(array, last_index, buf_value);
+
+	return new_pivot_index;
+error:
+	return -1;
+}
+
+static int DArray_quicksort_utility(DArray *array, int first_index, int last_index, DArray_compare cmp)
+{
+	check(array, "argument 'array' can't be NULL");
+
+	int new_pivot_index = 0;
+
+	if(first_index < last_index) {
+
+		new_pivot_index = DArray_partition(array, first_index, last_index, cmp);
+		DArray_quicksort_utility(array, first_index, new_pivot_index - 1, cmp);
+		DArray_quicksort_utility(array, new_pivot_index + 1, last_index, cmp);
+	}
+
+	return 0;
+error:
+	return -1;
+}
+
+int DArray_quicksort(DArray *array, DArray_compare cmp)
+{
+	return DArray_quicksort_utility(array, 0, DArray_count(array) - 1, cmp);
+}
---
+++ ex37/ec1/liblcthw/tests/hashmap_tests.c
@@ -0,0 +1,224 @@
+#include "minunit.h"
+#include <lcthw/hashmap.h>
+#include <assert.h>
+#include <lcthw/bstrlib.h>
+
+Hashmap *map = NULL;
+static int traverse_called = 0;
+struct tagbstring test1 = bsStatic("test data 1");
+struct tagbstring test2 = bsStatic("test data 2");
+struct tagbstring test3 = bsStatic("test data 3");
+struct tagbstring expect1 = bsStatic("THE VALUE 1");
+struct tagbstring expect2 = bsStatic("THE VALUE 2");
+struct tagbstring expect3 = bsStatic("THE VALUE 3");
+
+static uint32_t djb2_hash(void *a)
+{
+	char *key = bdata((bstring)a);
+
+	uint32_t hash = 5381;
+	int c;
+
+	while(c = *key++) {
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+
+	return hash;
+}
+
+static int nodes_distribution(Hashmap *map)
+{
+	int i = 0;
+
+	debug("NODES DISTRIBUTION:");
+	for(i = 0; i < DArray_count(map->buckets); i++) {
+		DArray *bucket = DArray_get(map->buckets, i);
+		if(bucket) {
+			debug("BUCKET %d:\t %d nodes", i, DArray_count(bucket));
+		}
+	}
+
+	return 0;
+}
+
+static int traverse_good_cb(HashmapNode *node)
+{
+	debug("KEY: %s", bdata((bstring)node->key));
+	traverse_called++;
+	return 0;
+}
+
+static int traverse_fail_cb(HashmapNode *node)
+{
+	debug("KEY: %s", bdata((bstring)node->key));
+	traverse_called++;
+
+	if(traverse_called == 2) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+char *test_create()
+{
+	map = Hashmap_create(NULL, NULL);
+	mu_assert(map != NULL, "Failed to create map.");
+
+	return NULL;
+}
+
+char *test_destroy()
+{
+	Hashmap_destroy(map);
+
+	return NULL;
+}
+
+char *test_get_set()
+{
+	int rc = Hashmap_set(map, &test1, &expect1);
+	mu_assert(rc == 0, "Failed to set &test1");
+	bstring result = Hashmap_get(map, &test1);
+	mu_assert(result == &expect1, "Wrong value for test1.");
+
+	rc = Hashmap_set(map, &test2, &expect2);
+	mu_assert(rc == 0, "Failed to set test2");
+	result = Hashmap_get(map, &test2);
+	mu_assert(result == &expect2, "Wrong value for test2");
+
+	rc = Hashmap_set(map, &test3, &expect3);
+	mu_assert(rc == 0, "Failed to set test3.");
+	result = Hashmap_get(map, &test3);
+	mu_assert(result == &expect3, "Wrong value for test3.");
+
+	nodes_distribution(map);
+
+	return NULL;
+}
+
+char *test_traverse()
+{
+	int rc = Hashmap_traverse(map, traverse_good_cb);
+	mu_assert(rc == 0, "Failed to traverse.");
+	mu_assert(traverse_called == 3, "Wrong count traverse.");
+
+	traverse_called = 0;
+	rc = Hashmap_traverse(map, traverse_fail_cb);
+	mu_assert(rc == 1, "Failed to traverse.");
+	mu_assert(traverse_called == 2, "Wrong count traverse for fail.");
+
+	return NULL;
+}
+
+char *test_delete()
+{
+	bstring deleted = (bstring)Hashmap_delete(map, &test1);
+	mu_assert(deleted != NULL, "Got NULL on delete.");
+	mu_assert(deleted == &expect1, "Should get test1");
+	bstring result = Hashmap_get(map, &test1);
+	mu_assert(result == NULL, "Should delete.");
+
+	deleted = (bstring)Hashmap_delete(map, &test2);
+	mu_assert(deleted != NULL, "Got NULL on delete.");
+	mu_assert(deleted == &expect2, "Should get test2");
+	result = Hashmap_get(map, &test2);
+	mu_assert(result == NULL, "Should delete.");
+
+	deleted	= (bstring)Hashmap_delete(map, &test3);
+	mu_assert(deleted != NULL, "Got NULL on delete.");
+	mu_assert(deleted == &expect3, "Should get test3");
+	result = Hashmap_get(map, &test3);
+	mu_assert(result == NULL, "Should delete.");
+	
+	return NULL;
+}
+
+char *test_rehash()
+{
+	Hashmap *map1 = Hashmap_create_advanced(NULL, djb2_hash, 1, 1);
+
+	mu_assert(map1->buckets_number == 1, "Wrong buckets number.");
+	
+	// set 1
+	Hashmap_set(map1, &test1, &expect1);
+
+	traverse_called = 0;
+	Hashmap_traverse(map1, traverse_good_cb);
+	mu_assert(traverse_called == 1, "Wrong count traverse.");	
+	mu_assert(map1->buckets_number == 1, "Wrong buckets number.");
+	nodes_distribution(map1);
+	debug("_______________________________________________");
+
+	// set 2
+	Hashmap_set(map1, &test2, &expect2);
+
+	traverse_called = 0;
+	Hashmap_traverse(map1, traverse_good_cb);
+	mu_assert(traverse_called == 2, "Wrong count traverse.");
+	mu_assert(map1->buckets_number == 2, "Wrong buckets number.");
+	nodes_distribution(map1);
+	debug("_______________________________________________");
+
+	// set 3
+	Hashmap_set(map1, &test3, &expect3);
+
+	traverse_called = 0;
+	Hashmap_traverse(map1, traverse_good_cb);
+	mu_assert(traverse_called == 3, "Wrong count traverse.");
+	mu_assert(map1->buckets_number == 3, "Wrong buckets number.");
+	nodes_distribution(map1);
+	debug("_______________________________________________");
+
+	// delete 1
+	Hashmap_delete(map1, &test1);
+
+	traverse_called = 0;
+	Hashmap_traverse(map1, traverse_good_cb);
+	mu_assert(traverse_called == 2, "Wrong count traverse.");
+	mu_assert(map1->buckets_number == 2, "Wrong buckets number.");
+	nodes_distribution(map1);
+	debug("_______________________________________________");
+
+	// delete 2
+	Hashmap_delete(map1, &test2);
+
+	traverse_called = 0;
+	Hashmap_traverse(map1, traverse_good_cb);
+	mu_assert(traverse_called == 1, "Wrong count traverse.");
+	mu_assert(map1->buckets_number == 1, "Wrong buckets number.");
+	nodes_distribution(map1);
+	debug("_______________________________________________");
+	
+	// delete 3
+	Hashmap_delete(map1, &test3);
+
+	traverse_called = 0;
+	Hashmap_traverse(map1, traverse_good_cb);
+	mu_assert(traverse_called == 0, "Wrong count traverse.");
+	mu_assert(map1->buckets_number == 1, "Wrong buckets number.");
+	nodes_distribution(map1);
+	debug("_______________________________________________");
+	
+	// destroy
+	Hashmap_destroy(map1);
+
+	return NULL;
+}
+
+char *all_tests()
+{
+	mu_suite_start();
+
+	mu_run_test(test_create);
+	mu_run_test(test_get_set);
+	mu_run_test(test_traverse);
+	mu_run_test(test_delete);
+	mu_run_test(test_destroy);
+	
+	mu_run_test(test_rehash);
+
+	return NULL;
+}
+
+RUN_TESTS(all_tests);
