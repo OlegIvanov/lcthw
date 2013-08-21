300f02e7fb671a486447b10c99d1285c0fbefef1
Oleg Ivanov <iosmsft@outlook.com>
2013-08-21 22:46:21 +0300

    ex41

--- ex41/liblcthw/tests/perf_tun.sh
+++ ex41/liblcthw/tests/perf_tun.sh
@@ -4,8 +4,8 @@ for i in tests/*_tests
 do
 	if test -f $i
 	then
-		valgrind --tool=callgrind --callgrind-out-file=callgring.out.$(basename $i) ./$i 2>> perf_tun.log
-		valgrind --tool=cachegrind --cachegrind-out-file=cachegring.out.$(basename $i) ./$i 2>> perf_tun.log
+		valgrind --tool=callgrind --callgrind-out-file=callgrind.out.$(basename $i) ./$i 2>> perf_tun.log
+		valgrind --tool=cachegrind --cachegrind-out-file=cachegrind.out.$(basename $i) ./$i 2>> perf_tun.log
 	fi
 done
 
