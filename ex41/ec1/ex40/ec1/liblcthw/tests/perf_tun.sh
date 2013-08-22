c120a6ca4ff41228e969ca3349f917c1f7868d8e
Oleg Ivanov <iosmsft@outlook.com>
2013-08-22 20:21:14 +0300

    ex41 ec1

---
+++ ex41/ec1/ex40/ec1/liblcthw/tests/perf_tun.sh
@@ -0,0 +1,12 @@
+echo "Getting callgrind and cachegrind out files:"
+
+for i in tests/*_tests
+do
+	if test -f $i
+	then
+		valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --callgrind-out-file=callgrind.out.$(basename $i) ./$i 2>> perf_tun.log
+		valgrind --tool=cachegrind --cachegrind-out-file=cachegrind.out.$(basename $i) ./$i 2>> perf_tun.log
+	fi
+done
+
+echo ""
